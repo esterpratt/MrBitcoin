4ce96c4bdace14b31adae6529294eff5909e3060
Ester Pratt <esterpratt@gmail.com>
2018-12-25 17:27:34 +0200

    Updates

---
+++ precache-manifest.ff792afda88bcab368af51095d9ff40e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1dced2920d4db2e2939f",
+    "url": "/MrBitcoin/static/css/main.0be72788.chunk.css"
+  },
+  {
+    "revision": "1dced2920d4db2e2939f",
+    "url": "/MrBitcoin/static/js/main.1dced292.chunk.js"
+  },
+  {
+    "revision": "5770c065a1f782bade53",
+    "url": "/MrBitcoin/static/js/1.5770c065.chunk.js"
+  },
+  {
+    "revision": "05411aefcd58722e4bd0",
+    "url": "/MrBitcoin/static/js/runtime~main.05411aef.js"
+  },
+  {
+    "revision": "0d9edfb85a0f980a6a10300331ac98da",
+    "url": "/MrBitcoin/index.html"
+  }
+];
